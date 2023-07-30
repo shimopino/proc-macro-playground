9ab433a3b543d73e388ef4fb8e21df7e8b608ee5
shimopino <shimoke4869@gmail.com>
2023-07-30 16:42:47 +0900

    feat(builder): add builder struct and builder method

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,30 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
 
-    println!("{:#?}", ast);
+    // println!("{:#?}", ast);
+    // TokenStream::new()
 
-    TokenStream::new()
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = syn::Ident::new(&bname, name.span());
+    let expanded = quote! {
+        struct #bident {
+
+        }
+
+        impl #name {
+            fn builder() -> #bident {
+                #bident {
+
+                }
+            }
+        }
+    };
+
+    expanded.into()
 }
