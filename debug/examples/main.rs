400b98d8e9a397446dc6d44b0dc34bb63dcfb912
shimopino <shimoke4869@gmail.com>
2023-08-21 21:45:25 +0900

    feat: attrをアンラップする関数を追加

--- debug/examples/main.rs
+++ debug/examples/main.rs
@@ -3,7 +3,24 @@ use derive_debug::CustomDebug;
 #[derive(CustomDebug)]
 pub struct Field {
     name: &'static str,
-    bitmask: u16,
+    #[debug = "0b{:08b}"]
+    bitmask: u8,
 }
 
-fn main() {}
+// impl std::fmt::Debug for Field {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         f.debug_struct("Field")
+//             .field("name", &self.name)
+//             .field("bitmask", &format_args!("{:08b}", &self.bitmask))
+//             .finish()
+//     }
+// }
+
+fn main() {
+    let f = Field {
+        name: "F",
+        bitmask: 0b00011100,
+    };
+
+    println!("{:?}", f);
+}
--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,35 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, spanned::Spanned, DeriveInput};
 
-#[proc_macro_derive(CustomDebug)]
+fn unwrap_debug_attribute(
+    attrs: &[syn::Attribute],
+) -> Result<Vec<String>, proc_macro2::TokenStream> {
+    let mut attrs_values = vec![];
+    for attr in attrs {
+        match attr.parse_args::<syn::MetaNameValue>() {
+            Ok(named) if named.path.is_ident("debug") => {
+                if let syn::Expr::Lit(syn::ExprLit {
+                    lit: syn::Lit::Str(ref liststr),
+                    ..
+                }) = named.value
+                {
+                    attrs_values.push(liststr.value())
+                }
+            }
+            Ok(_) => {
+                return Err(
+                    syn::Error::new(attr.span(), "only debug attributes can be applied")
+                        .to_compile_error(),
+                )
+            }
+            Err(err) => return Err(err.to_compile_error()),
+        }
+    }
+    Ok(attrs_values)
+}
+
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let parsed = parse_macro_input!(input as DeriveInput);
 
